262aef5ede42dd06053c50230379102cdb35b5f5
unknown <jesusalexis.rz@gmail.com>
2019-12-09 11:05:28 -0700

    leve modificacion index

--- index.php
+++ index.php
@@ -148,18 +148,13 @@ if ($_SESSION['usuarioNivel']==1) {
                   <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                       <span class="navbar-toggler-icon"></span>
                   </button>
-                  <a class="navbar-brand" href="marketing-index.html"><img src="images/version/market-logo.png" alt=""></a>
+                  <a class="navbar-brand" href="index.php"><img src="images/version/market-logo.png" alt=""></a>
                   <div class="collapse navbar-collapse" id="navbarCollapse">
                       <ul class="navbar-nav mr-auto">
                           <li class="nav-item">
                               <a class="nav-link" href="marketing-index.html">Inicio</a>
                           </li>
-                          <li class="nav-item">
-                              <a class="nav-link" href="categoria.php">Categorias</a>
-                          </li>
-                          <li class="nav-item">
-                              <a class="nav-link" href="marketing-category.html">Compras</a>
-                          </li>
+                      
                           <li class="nav-item">
                               <a class="nav-link" href="marketing-contact.html">Contactanos</a>
                           </li>
