10f4042cc0dd7c9c6aca1ad83ad39c51ef1fc2d9
Asus <jesusalexis.rz@gmail.com>
2019-12-12 11:26:12 -0700

    Se agrego la base de datos

--- base de datos/proyectoalonso.sql
+++ base de datos/proyectoalonso.sql
@@ -2,10 +2,10 @@
 -- version 4.9.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 19-11-2019 a las 21:14:03
+-- Servidor: localhost
+-- Tiempo de generación: 12-12-2019 a las 19:23:24
 -- Versión del servidor: 10.4.8-MariaDB
--- Versión de PHP: 7.1.32
+-- Versión de PHP: 7.3.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -45,14 +45,27 @@ CREATE TABLE `comentario` (
 
 CREATE TABLE `producto` (
   `idproducto` int(11) NOT NULL,
-  `Imagen` varchar(500) CHARACTER SET utf8mb4 NOT NULL,
+  `Imagen` mediumtext CHARACTER SET utf8mb4 NOT NULL,
   `Titulo` varchar(30) CHARACTER SET utf8mb4 NOT NULL,
   `Descripcion` varchar(60) CHARACTER SET utf8mb4 NOT NULL,
   `Precio` double NOT NULL,
-  `idUsuariof` int(11) NOT NULL,
   `Categoria` int(11) DEFAULT NULL,
   `Stock` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_bin;
+
+--
+-- Volcado de datos para la tabla `producto`
+--
+
+INSERT INTO `producto` (`idproducto`, `Imagen`, `Titulo`, `Descripcion`, `Precio`, `Categoria`, `Stock`) VALUES
+(1, 'https://cdn.shopify.com/s/files/1/1640/7017/products/zapatos_nov_15.jpg?v=1575322046', 'Zapato de payaso', 'Zapato de payaso blanco con rojo', 650, 1, 3),
+(2, 'https://www.abc.es/media/summum/2018/05/05/GeorgeCleverley1250-kcdD-U213133674154ZJI-350x350@abc.jpg', 'Zapato de vestir', 'Zapato con suela de goma y muy buena absorcion de impacto co', 850, 1, 3),
+(3, 'https://resources.sears.com.mx/medios-plazavip/fotos/productos_sears1/original/2903745.jpg', 'Blusa rosa', 'Blusa rosa para dama', 400, 2, 2),
+(4, 'https://images-na.ssl-images-amazon.com/images/I/61OkKGdngcL._UX679_.jpg', 'Blusa blanca 3/4', 'Blusa blanca para verano 3/4 marca class', 400, 2, 1),
+(5, 'https://i.linio.com/p/daed2ed4562cf7ca33b82ae563babc82-product.jpg', 'Pantalon', 'Pantalon de mezclilla para caballero levis', 450, 3, 2),
+(6, 'https://i.pinimg.com/236x/76/52/8a/76528a1cfa8fad3e854427a06f46f048.jpg', 'Pantalon mezclilla dama', 'Pantalon de mezclilla para dama levis', 500, 3, 3),
+(7, 'https://resources.sears.com.mx/medios-plazavip/fotos/productos_sears1/original/2932684.jpg', 'Vestido coral', 'Vestido color coral, marca cklass', 800, 4, 1),
+(8, 'https://asset4.zankyou.com/images/mag-card-t/753/6cd0/350/350/-/es/wp-content/uploads/2018/02/2t189-rosa-clara.jpg', 'Vestido negro con blanco', 'Vestido negro con blanco para dama', 1400, 4, 2);
 
 -- --------------------------------------------------------
 
@@ -83,6 +96,14 @@ CREATE TABLE `usuario` (
   `Contraseña` varchar(96) CHARACTER SET utf8mb4 NOT NULL,
   `Direccion` varchar(40) CHARACTER SET utf8mb4 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_bin;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`idUsuario`, `Nivel`, `Nombre`, `Correo`, `Contraseña`, `Direccion`) VALUES
+(5, '3', 'usr', 'alexis@gmail.com', '123', '123'),
+(9, '2', 'pepe pedro', 'pepe@gmail.com', '123', 'por hay');
 
 -- --------------------------------------------------------
 
@@ -146,7 +167,7 @@ ALTER TABLE `comentario`
 -- AUTO_INCREMENT de la tabla `producto`
 --
 ALTER TABLE `producto`
-  MODIFY `idproducto` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idproducto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de la tabla `tarjeta`
@@ -158,7 +179,7 @@ ALTER TABLE `tarjeta`
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT de la tabla `ventas`
